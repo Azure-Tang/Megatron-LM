fix bug about attention mask

megatron/core/pipeline_parallel/schedules.py
@@ -250,7 +250,7 @@ def forward_step_terapipe(
     set_input_tensor = get_attr_wrapped_model(model, "set_input_tensor")
     set_input_tensor(input_tensor)
 
-    if config.enable_autocast: # @@@false
+    if config.enable_autocast:
         context_manager = torch.autocast("cuda", dtype=config.autocast_dtype)
     else:
         context_manager = contextlib.nullcontext()

megatron/model/fused_softmax.py
@@ -144,7 +144,6 @@ class FusedScaleMaskSoftmax(nn.Module):
     def forward(self, input, mask):
         # [b, np, sq, sk]
         assert input.dim() == 4
-
         if self.is_kernel_available(mask, *input.size()):
             return self.forward_fused_softmax(input, mask)
         else:
@@ -152,6 +151,8 @@ class FusedScaleMaskSoftmax(nn.Module):
 
     def is_kernel_available(self, mask, b, np, sq, sk):
         attn_batches = b * np
+
+
 
         if (
             self.scaled_masked_softmax_fusion  # user want to fuse

megatron/model/transformer_cache.py
@@ -264,7 +264,6 @@ class CoreAttention(MegatronModule):
 
     def forward(self, query_layer, key_layer,
                 value_layer, attention_mask):
-
         # ===================================
         # Raw attention scores. [b, np, s, s]
         # ===================================
@@ -716,7 +715,7 @@ class ParallelAttentionWithCache(MegatronModule):
             value_layer = cache_v[:src_len, :, :, :]
             # adjust attention_mask to fit the new key_layer
             # [1, 1, sq, sk] -> [1, 1, sq, sk + cache_len]
-            cache_mask = torch.full((1, 1, attention_mask.size(2), cache_len), False, dtype=attention_mask.dtype, device=attention_mask.device)
+            cache_mask = torch.full((1, 1, attention_mask.size(2), cache_len), True, dtype=attention_mask.dtype, device=attention_mask.device)
             # Concatenate the cache mask with the original mask
             attention_mask = torch.cat([cache_mask, attention_mask], dim=-1)
 
@@ -743,7 +742,6 @@ class ParallelAttentionWithCache(MegatronModule):
             # absolute positional embedding.
             # otherwise, only relative positional embedding takes effect
             # value_layer = apply_rotary_pos_emb(value_layer, k_pos_emb)
-
         if not self.use_flash_attn:
             # for recompute
             if self.checkpoint_core_attention:
@@ -1736,7 +1734,6 @@ class ParallelTransformerWithCache(MegatronModule):
                         forward_kwargs['cache'] = (k, v)
                         forward_kwargs['cache_seq_len'] = cache_seq_len
                         layer = self._get_layer(index)
-
                         hidden_states, (k_output, v_output) = layer(
                             hidden_states,
                             attention_mask,

pretrain_gpt_terapipe.py
@@ -75,12 +75,12 @@ def get_batch(data_iterator):
     return tokens, labels, loss_mask, attention_mask, position_ids
 
 def loss_func(loss_mask, output_tensor):
+    args = get_args()
     losses = output_tensor.float()
     loss_mask = loss_mask.view(-1).float()
     loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()
 
     # Check individual rank losses are not NaN prior to DP all-reduce.
-    args = get_args()
     if args.check_for_nan_in_loss_and_grad:
         global_rank = torch.distributed.get_rank()
         assert not loss.isnan(), (
@@ -104,6 +104,8 @@ def forward_step(data_iterator, model, cache, cache_seq_len=0):
     tokens, labels, loss_mask, attention_mask, position_ids = get_batch(
         data_iterator)
     timers('batch-generator').stop()
+    from megatron import gpu_logger
+    gpu_logger(f'loss_mask.shape:{loss_mask.shape} \n loss_mask:{loss_mask} \n attention_mask.shape:{attention_mask.shape} \n attention_mask:{attention_mask}')
 
     output_tensor, cache_output = model(tokens, position_ids, attention_mask,
                           labels=labels, cache=cache, cache_seq_len=cache_seq_len)